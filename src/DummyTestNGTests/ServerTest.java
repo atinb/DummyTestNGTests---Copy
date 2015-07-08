6c15c4a6e187669b47e4e7c34f11b4682823eac7
Ajay Arora <ajarora@microsoft.com>
2015-07-08 06:26:19 -0700

    Change to JUnit format

--- src/DummyTestNGTests/ServerTest.java
+++ src/DummyTestNGTests/ServerTest.java
@@ -1,55 +1,20 @@
-package DummyTestNGTests;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
+package mypackage.files;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class ServerTest {
-
-	@Test
-
-    public void ServerTest1()
-    {
-        String str = "Firefox BVT";
-        try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        Assert.assertEquals(str, "Firefox BVT");
-       	
-    }
-
-    @Test
-    public void ServerTest2()
-    {
-        Assert.assertEquals(2, 2);
-    	
-    }
-
-    @Test
-    public void ServerTest3()
-    {
-    	Assert.assertEquals(3, 3);
-    }
-
-    @Test
-    public void ServerTest4()
-    {
-    	Assert.assertEquals(4, 4);
-    }
-
-    @Test
-    public void ServerTest5()
-    {
-    	Assert.assertEquals(5, 5);
-        try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-    }	
-	
-
+   @Test
+   public void testA() {
+      String str= "abc";
+      assertEquals("abc",str);
+   }
+   @Test
+   public void testB() {
+      String str= "xyzth";
+      assertEquals("zdf",str);
+   }
+   @Test
+   public void testC() {
+      String str= "mno";
+      assertEquals("mo",str);
+   }
 }
