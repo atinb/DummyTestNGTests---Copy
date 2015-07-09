79557c5abf36655970aa9074e3735cb5ddfe9c13
Ajay Arora <ajarora@microsoft.com>
2015-07-09 04:21:15 -0700

    all sliced xmls

---
+++ src/DummyTestNGTests1/ClientTest.java
@@ -0,0 +1,20 @@
+package mypackage.files;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class ClientTest {
+   @Test
+   public void testA() {
+      String str= "abc";
+      assertEquals("abc",str);
+   }
+   @Test
+   public void testB() {
+      String str= "xyzth";
+      assertEquals("zdf",str);
+   }
+   @Test
+   public void testC() {
+      String str= "mno";
+      assertEquals("mo",str);
+   }
+}
